50d1a838fc489cdedda61f7a48bfcac16be3616d
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-05-18 08:56:27 -0400

    Added comments

--- scripts/babel-wg/mcast-announce-wg.sh
+++ scripts/babel-wg/mcast-announce-wg.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Load wireguard variables. specifically $publicKey
 source /etc/wg
 
+# Infinate loop
 while true; do
+  # iterate through interfaces
   for int in $(find /sys/class/net/* -maxdepth 1 -print0 | xargs -0 -l basename); do
-    if [[ "$int" == "eth"* ]]; then
-      echo "WG|$publicKey" |  socat - UDP6-datagram:[ff02::1%$int]:1234
-    fi
-    if [[ "$int" == "wlan"* ]]; then
-      echo "WG|$publicKey" |  socat - UDP6-datagram:[ff02::1%$int]:1234
+    # only broadcast on eth and wlan interfaces
+    if [[ "$int" == "eth"* || "$int" == "wlan"* ]] ; then
+      # Announce wireguard's existanse and it's public key
+      echo "WG|$publicKey" | socat - UDP6-datagram:[ff02::1%$int]:1234
     fi
   done
+  # Repeat every 30 seconds (heartbeat)
   sleep 30
 done
